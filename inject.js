efdb15314b48a720b77c170680947411ce9c8496
VidFerris <29590790+VidFerris@users.noreply.github.com>
2021-04-18 08:46:13 +1000

    Updated for New TickTick
    .css file now includes fonts as a DataURL so no need for external request (which sometimes broke), JS now includes ctrl-F selection of the search box, various other tweaks

--- inject.js
+++ inject.js
@@ -1,3 +1,7 @@
+
+//Schedule Refresh and Backup
+var prevloc;
+
 function refreshAt(hours, minutes, seconds) {
     var now = new Date();
     var then = new Date();
@@ -15,7 +19,28 @@ function refreshAt(hours, minutes, seconds) {
     setTimeout(function() { window.location.reload(true); }, timeout);
 }
 
-refreshAt(0,5,0); //Will refresh the page at 1am
+function backupAt(hours, minutes, seconds) {
+    var now = new Date();
+    var then = new Date();
+
+    if(now.getHours() > hours ||
+       (now.getHours() == hours && now.getMinutes() > minutes) ||
+        now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+        then.setDate(now.getDate() + 1);
+    }
+    then.setHours(hours);
+    then.setMinutes(minutes);
+    then.setSeconds(seconds);
+
+    var timeout = (then.getTime() - now.getTime());
+    setTimeout(function() { prevloc = window.location.href; window.location.href = "https://ticktick.com/webapp/#settings/backup";}, timeout);
+	setTimeout(function() { document.querySelectorAll('[id="btn-generate"]')[0].click();}, timeout+3000);
+	setTimeout(function() { window.location.href = prevloc;}, timeout+60000);
+}
+
+backupAt(0, 1, 0); //Will backup at 12:01.
+
+refreshAt(0,5,0); //Will refresh the page at 12:05.
 
 
 setTimeout(initLowerRight, 1000*5);
@@ -114,11 +139,28 @@ var br = document.createElement('br');
 br.innerText = '';
 nkr.appendChild(br);
 
-updateTimer();
+setInterval(updateTimer, 100);
 
-setTimeout(showTime, 1000);
+setInterval(showTime, 1000);
 }
 
+
+
+
+
+
+a=new AudioContext()
+function beep(vol, freq, duration, delay){
+  v=a.createOscillator()
+  u=a.createGain()
+  v.connect(u)
+  v.frequency.value=freq
+  v.type="square"
+  u.connect(a.destination)
+  u.gain.value=vol*0.01
+  v.start(a.currentTime+delay*0.001)
+  v.stop(a.currentTime+duration*0.001+delay*0.001)
+}
 
 
 
@@ -139,6 +181,7 @@ var mins = 0;
 var secs = 0;
 var secsS = "";
 var width = 1;
+var alarmDone = 1;
 
 
 
@@ -146,8 +189,11 @@ function newTimer(dur,col) {
 	startTime = new Date();
     bar.style.backgroundColor = col;
     duration = dur;
+	alarmDone = 0;
 }
 
+
+var newtime = "";
 
 function updateTimer() {
 	currentTime = new Date();
@@ -162,10 +208,25 @@ function updateTimer() {
     	secsS = secs;
     }
     mins = Math.floor(rem/60);
-    remaining.innerHTML = mins + ":" + secsS;
-    setTimeout(updateTimer, 10);
+	newTime = mins + ":" + secsS;
+	if(remaining.innerHTML!=newTime){
+		remaining.innerHTML = newTime;
+	}
+	
+	if(rem==0&&alarmDone==0){
+		alarmDone = 1;
+		beep(50,700,200,400);
+		beep(50,500,200,200);
+		beep(50,300,200,0);
+		beep(50,600,400,600);
+		beep(50,500,400,600);
+		beep(50,400,400,600);
+		beep(50,500,150,1000);
+		beep(50,400,150,1000);
+		beep(50,300,150,1000);
+		setTimeout(function() { alert('Timer Finished!'); }, 1500);
+	}
 }
-
 
 
 
@@ -215,13 +276,57 @@ var mon = month[date.getMonth()];
     s = (s < 10) ? "0" + s : s;
     
     var time = m + " " + s;
-    document.getElementById("hr").innerText = h;
-    document.getElementById("hr").textContent = h;
-    document.getElementById("min").innerText = m;
-    document.getElementById("min").textContent = m;
-    document.getElementById("wk").innerText = n;
-    document.getElementById("wk").textContent = n;
-    
-    setTimeout(showTime, 1000);
+	if(document.getElementById("hr").textContent != h){
+		document.getElementById("hr").textContent = h;
+	}
+    //document.getElementById("hr").textContent = h;
+	if(document.getElementById("min").textContent != m){
+		document.getElementById("min").textContent = m;
+	}
+    //document.getElementById("min").textContent = m;
+	if(document.getElementById("wk").textContent != n){
+		document.getElementById("wk").textContent = n;
+	}
+    //document.getElementById("wk").textContent = n;
     
 }
+
+//Ctrl-F Selects Search Box
+function KeyPress(e) {
+      var evtobj = window.event? event : e
+      if (evtobj.keyCode == 70 && evtobj.ctrlKey) document.getElementsByClassName("search-box")[0].focus();
+}
+
+//Need to wait until loaded to add the key capturing code for shortcuts
+function captureKeys(){
+	document.onkeydown = KeyPress;
+}
+setTimeout(captureKeys, 1000*5);
+
+
+
+
+
+
+
+
+//Reload Image Regularly
+
+function reloadBackground() {
+document.getElementById("right-view").style.backgroundImage = "url('https://source.unsplash.com/random/440x860/?t="+new Date().getTime()+"')"
+//document.getElementById("right-view").style.backgroundImage = "url('https://source.unsplash.com/random/440x860/?bonk')"
+//document.getElementById("right-view").style.backgroundImage = "url('https://source.unsplash.com/random/440x860/')"
+}
+//Once Per 4 Hours
+setInterval(reloadBackground,4*60*60*1000)
+
+
+
+
+
+//Insert Frame into Bike Task
+//bframe = document.querySelectorAll('a[href*="http://192.168.1.205:1880/live"]')[0].insertBefore(document.createElement("iframe"),null)
+//bframe.setAttribute("src", "https://192.168.1.205:1880/live");
+//bframe.style.width = "300px";
+//bframe.style.height = "30px";
+
